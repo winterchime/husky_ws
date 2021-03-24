bda18ee9919c95e0e9daa98bdac9e56fa7b89b76
Jaime Gonzalez <jegonzalez17@yahoo.com>
2021-03-23 20:40:00 -0700

    3/23/21

---
+++ build/husky_control/CMakeFiles/husky_control.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/husky_control.dir/teleop_twist_keyboard.cpp.o"
+  "/home/mitto90/husky_ws/devel/lib/husky_control/husky_control.pdb"
+  "/home/mitto90/husky_ws/devel/lib/husky_control/husky_control"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/husky_control.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
